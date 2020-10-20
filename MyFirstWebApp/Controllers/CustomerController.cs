b4cc2e052cae50ea8298d9b697ac5da2352aadf9
Eunbee Kim <ekim105@myseneca.ca>
2020-10-20 19:28:37 -0400

    first commit

---
+++ MyFirstWebApp/Controllers/CustomerController.cs
@@ -0,0 +1,143 @@
+﻿using MyFirstWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private Manager m = new Manager();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var c = m.CustomerGetAll();
+
+            return View(c);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            // Attempt to get the matching object
+            var obj = m.CustomerGetById(id.GetValueOrDefault());
+            if (obj == null)
+                return HttpNotFound();
+            else
+                return View(obj);
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(CustomerAddViewModel newItem)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(newItem);
+            }
+
+            try
+            {
+                // TODO: Add insert logic here
+                var addedItem = m.CustomerAdd(newItem);
+
+                if(addedItem == null)
+                {
+                    return View(newItem);
+                }
+                else
+                {
+                    return RedirectToAction("Details", new { id = addedItem.CustomerId });
+                }
+               
+            }
+            catch
+            {
+                return View(newItem);
+            }
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            // Attempt to fetch the matching object
+            var obj = m.CustomerGetById(id.GetValueOrDefault());
+            if (obj == null)
+                return HttpNotFound();
+            else
+            {
+                // Create and configure an "edit form"
+                // Notice that obj is a CustomerBaseViewModel object so
+                // we must map it to a CustomerEditContactFormViewModel object
+                // Notice that we can use AutoMapper anywhere,
+                // and not just in the Manager class.
+                var formObj = m.mapper.Map<CustomerBaseViewModel, CustomerEditContactFormViewModel>(obj);
+                return View(formObj);
+            }
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int? id, CustomerEditContactViewModel model)
+        {
+            // Validate the input
+            if (!ModelState.IsValid)
+            {
+                // Our "version 1" approach is to display the "edit form" again
+                return RedirectToAction("Edit", new { id = model.CustomerId });
+            }
+            if (id.GetValueOrDefault() != model.CustomerId)
+            {
+                // This appears to be data tampering, so redirect the user away
+                return RedirectToAction("Index");
+            }
+            // Attempt to do the update
+            var editedItem = m.CustomerEditContactInfo(model);
+            if (editedItem == null)
+            {
+                // There was a problem updating the object
+                // Our "version 1" approach is to display the "edit form" again
+                return RedirectToAction("Edit", new { id = model.CustomerId });
+            }
+            else
+            {
+                // Show the details view, which will show the updated data
+                return RedirectToAction("Details", new { id = model.CustomerId });
+            }
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            var itemToDelete = m.CustomerGetById(id.GetValueOrDefault());
+            if (itemToDelete == null)
+            {
+                // Don't leak info about the delete attempt
+                // Simply redirect
+                return RedirectToAction("Index");
+            }
+            else
+                return View(itemToDelete);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int? id, FormCollection collection)
+        {
+            var result = m.CustomerDelete(id.GetValueOrDefault());
+            // "result" will be true or false
+            // We probably won't do much with the result, because
+            // we don't want to leak info about the delete attempt
+            // In the end, we should just redirect to the list view
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyFirstWebApp/Controllers/Manager.cs
@@ -0,0 +1,108 @@
+﻿// **************************************************
+// WEB524 Project Template V1 == b9210814-4f17-4492-a60c-272aa5dc9699
+// Do not change this header.
+// **************************************************
+
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyFirstWebApp.EntityModels;
+using MyFirstWebApp.Models;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        // AutoMapper instance
+        public IMapper mapper;
+
+        public Manager()
+        {
+            // If necessary, add more constructor code here...
+
+            // Configure the AutoMapper components
+            var config = new MapperConfiguration(cfg =>
+            {
+                // Define the mappings below, for example...
+                // cfg.CreateMap<SourceType, DestinationType>();
+                // cfg.CreateMap<Employee, EmployeeBase>();
+                cfg.CreateMap<Customer, CustomerBaseViewModel>();
+                cfg.CreateMap<CustomerAddViewModel, Customer>();
+                cfg.CreateMap<CustomerBaseViewModel, CustomerEditContactFormViewModel>();
+            });
+
+            mapper = config.CreateMapper();
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // Add your methods and call them from controllers.  Use the suggested naming convention.
+        // Ensure that your methods accept and deliver ONLY view model objects and collections.
+        // When working with collections, the return type is almost always IEnumerable<T>.
+
+        public IEnumerable<CustomerBaseViewModel> CustomerGetAll()
+        {
+            return mapper.Map<IEnumerable<Customer>, IEnumerable<CustomerBaseViewModel>>(ds.Customers);
+        }
+
+        public CustomerBaseViewModel CustomerGetById(int id)
+        {
+            var obj = ds.Customers.Find(id);
+            return obj == null ? null : mapper.Map<Customer, CustomerBaseViewModel>(obj);
+        }
+
+        public CustomerBaseViewModel CustomerAdd(CustomerAddViewModel newCustomer)
+        {
+            var addedItem = ds.Customers.Add(mapper.Map<CustomerAddViewModel, Customer>(newCustomer));
+            ds.SaveChanges();
+
+            return addedItem == null ? null : mapper.Map<Customer, CustomerBaseViewModel>(addedItem);
+        }
+
+        public CustomerBaseViewModel CustomerEditContactInfo(CustomerEditContactViewModel customer)
+        {
+            // Attempt to fetch the object.
+            var obj = ds.Customers.Find(customer.CustomerId);
+            if (obj == null)
+            {
+                // Customer was not found, return null.
+                return null;
+            }
+            else
+            {
+                // Customer was found. Update the entity object
+                // with the incoming values then save the changes.
+                ds.Entry(obj).CurrentValues.SetValues(customer);
+                ds.SaveChanges();
+                // Prepare and return the object.
+                return mapper.Map<Customer, CustomerBaseViewModel>(obj);
+            }
+        }
+
+        public bool CustomerDelete(int id)
+        {
+            // Attempt to fetch the object to be deleted
+            var itemToDelete = ds.Customers.Find(id);
+            if (itemToDelete == null)
+                return false;
+            else
+            {
+                // Remove the object
+                ds.Customers.Remove(itemToDelete);
+                ds.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
